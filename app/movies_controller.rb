83573e35d4cb99e17327b8d448dab122184da28f
joey <pball626@gmail.com>
2021-03-06 20:43:34 -0600

    commit tree cfeb4dafe38edabc14bc43c127fce0b9c5bf90d1

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,21 +6,21 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
   # Initialize movie and then and save it
-  attributes = {
-      title: "The Sting",
-      release_date: 1973,
-      director: "George Roy Hill",
-      lead: "Paul Newman",
-      in_theaters: false
-  }
-  movie = __
+  # attributes = {
+  #     # title: "The Sting",
+  #     # release_date: 1973,
+  #     # director: "George Roy Hill",
+  #     # lead: "Paul Newman",
+  #     # in_theaters: false
+  # }
+  movie = Movie.create(attributes)
 end
 
 def can_be_created_in_a_block(args = __)
